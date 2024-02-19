90087a061d17897c88fb1971ce2be04f0dae67ab
Stephen <vinosteev2019@gmail.com>
2024-02-19 15:58:00 +0100

    first commit

---
+++ src/components/product/Product.js
@@ -0,0 +1,21 @@
+
+import React from 'react';
+import "./Product.css";
+// import productImg from "../../images/7.png"
+
+const Product = ({title, img, price}) => {
+  return (
+    <div className='product --card'>
+      <img src={img} alt='product image' />
+      <div className='bg-primary --center-all --p'>
+        <h3 className='--text-light'>{title}</h3>
+        <div className='--flex-between --width-100'>
+          <p className='--text-light'>{price}</p>
+          <button className='--btn --btn-danger'>Add to Cart</button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Product
